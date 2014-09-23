2f1532ffdf8b1b1c5a3242f5a33c64bb8c779dfb
1 <1@1-PC>
2014-09-23 12:15:10 +0900

    초기소스.

---
+++ J20140923/src/생성자메인.java
@@ -0,0 +1,25 @@
+
+public class 생성자메인 {
+
+	public 생성자메인()
+	{
+		System.out.println("Test");
+	}
+	public 생성자메인(String str)  //생성자 오버로딩
+	{
+		System.out.println("Test");
+	}	
+	
+	public void 생성자메인()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+//		생성자메인 sm = new 생성자메인();
+//		생성자메인 sm1 = new 생성자메인();
+		생성자 con = new 생성자();
+
+	}
+
+}
